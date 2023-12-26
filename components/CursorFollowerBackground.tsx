572b3c52e86f7cc88c82ae3c001135d8b535b6fb
Oprișor Raul-Alexandru <oprisorraul@gmail.com>
2023-12-27 01:08:03 +0200

    Fixed CursorFollowerBackground to work after scroll

--- components/CursorFollowerBackground.tsx
+++ components/CursorFollowerBackground.tsx
@@ -1,33 +1,30 @@
 "use client";
-
 import React, { useState, useEffect } from "react";
 
-const CursorFollowerBackground = ({}: {}) => {
+const CursorFollowerBackground = () => {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
-      setCursorPosition({ x: event.clientX, y: event.clientY });
+      const x = event.pageX - window.scrollX;
+      const y = event.pageY - window.scrollY;
+
+      setCursorPosition({ x, y });
     };
 
-    window.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mousemove", handleMouseMove);
 
     return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
+      document.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
 
-  const [cursorPosition, setCursorPosition] = useState({
-    x: typeof window !== "undefined" ? window.innerWidth / 2 : 0,
-    y: typeof window !== "undefined" ? window.innerHeight / 2 : 0,
-  });
-
-  const backgroundStyle = {
-    background: `radial-gradient(600px at ${cursorPosition.x}px ${cursorPosition.y}px, rgba(162, 145, 219, 0.06), transparent 55%)`,
-  } as React.CSSProperties;
-
   return (
     <div
-      className="pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute"
-      style={backgroundStyle}
+      className="fixed inset-0 z-30 pointer-events-none transition duration-300"
+      style={{
+        background: `radial-gradient(600px at ${cursorPosition.x}px ${cursorPosition.y}px, rgba(162, 145, 219, 0.06), transparent 55%)`,
+      }}
     />
   );
 };
