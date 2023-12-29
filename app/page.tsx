4e900c1242057d4c074a6b496f5d56af43356838
Oprișor Raul-Alexandru <oprisorraul@gmail.com>
2023-12-29 14:54:48 +0200

    Removed main

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import CursorFollowerBackground from "@/components/CursorFollowerBackground";
 import Header from "@/components/welcomePage/header/Header";
-import Main from "@/components/welcomePage/main/Main";
+import About from "@/components/welcomePage/main/About";
+import Experience from "@/components/welcomePage/main/Experience";
+import Projects from "@/components/welcomePage/main/Projects";
 
 export default function Home() {
   return (
     <div className="lg:flex lg:justify-between lg:gap-4">
       <Header />
-      <Main />
+      <main id="content" className="pt-24 lg:w-1/2 lg:py-24">
+        <About />
+        <Experience />
+        <Projects />
+      </main>
       <CursorFollowerBackground />
     </div>
   );
