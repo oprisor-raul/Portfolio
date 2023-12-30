0527deaa59a5831f5314b16d5bebe89c9b177c65
Oprișor Raul-Alexandru <oprisorraul@gmail.com>
2023-12-30 23:47:14 +0200

    updated CV

--- app/resume.pdf/page.tsx
+++ app/resume.pdf/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Resume = () => {
   return (
     <iframe
-      src="https://files.oprisor.dev/api/public/dl/wRgCUNr8?inline=true"
+      src="https://files.oprisor.dev/api/public/dl/XeyJXiCz?inline=true"
       style={{ width: "100%", height: "100vh" }}
     />
   );
